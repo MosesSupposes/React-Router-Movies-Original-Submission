57c61869fdc91276b3fc9cfb9d143e31987bcd41
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 15:13:30 -0700

    make text on SavedList link back to the movie it represents

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,7 +11,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map((movie, i) => (
-          <span key={i} className="saved-movie">{movie.title}</span>
+          <Link to={`/movies/${movie.id}`}>
+            <span key={i} className="saved-movie">{movie.title}</span>
+          </Link>
         ))}
         <Link to="/">
           <div className="home-button">Home</div>
