33968806ec82179f682a599d95ea31f09b00bece
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 13:46:03 -0700

    add index route and '/movies/id' route to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
@@ -22,7 +23,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route path="/" component={MovieList}></Route>
+        <Route path="/movies/:id" component={Movie}></Route>
       </div>
     );
   }
