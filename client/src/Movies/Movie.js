6d71d5a46f288760b1fd87d3ed641938e470bcae
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 14:18:02 -0700

    make Movie component fetch/render new data on url change

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -25,12 +25,12 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
   // saveMovie = () => {
   //   const addToSavedList = this.props.addToSavedList;
