1e5a45e621a114eae1fcfa792cb29406e4e0848a
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 14:12:36 -0700

    add dynamic Link to MovieCard components

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  return (
+    <Link to={`/movies/${props.movie.id.toString()}`}>
+      {props.children}
+    </Link>
+  )
 };
 
 export default MovieCard;
