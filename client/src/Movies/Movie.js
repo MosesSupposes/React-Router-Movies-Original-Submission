e2023d6c41b2e051d3c2a9cc44bd3fae56befb20
Moses Samuel <mosamuel98@gmail.com>
2019-05-06 14:15:00 -0700

    Movie component now grabs the id off the url when it mounts

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,7 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
