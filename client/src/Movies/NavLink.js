bca6ddb7608509bd81b7eb2c14aebe0d542e4f2c
Moses Samuel <mosamuel98@gmail.com>
2019-05-07 11:37:41 -0700

    refactor: use actual NavLink component from React Router

--- client/src/Movies/NavLink.js
+++ client/src/Movies/NavLink.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink as RRNavLink  } from 'react-router-dom'
 
 export default function NavLink({removeFromSavedList, movie}) {
     return (
-        <Link to={`/movies/${movie.id}`}>
+        <RRNavLink to={`/movies/${movie.id}`}>
             <span 
                 className="saved-movie"
                 onDoubleClick={removeFromSavedList.bind(null, movie)}
             >
                 {movie.title}
             </span>
-        </Link>
+        </RRNavLink>
     )
 }
